2edd9f49a7c1c8161001d16b4a4bc03421c789ac
Burhanuddin <buscbd@gmail.com>
2024-02-17 17:30:19 +0600

    added grand total value

---
+++ script.js
@@ -0,0 +1,58 @@
+let count = 0;
+const allBtn = document.getElementsByClassName("add-btn");
+for (const btn of allBtn) {
+  btn.addEventListener("click", function (e) {
+    count += 1;
+    const placeName = e.target.parentNode.childNodes[1].innerText;
+    const price = e.target.parentNode.childNodes[3].childNodes[1].innerText;
+    const selectedContainer = document.getElementById('selected-place-container');
+    
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.innerText = placeName;
+    const p2 = document.createElement('p');
+    p2.innerText = price;
+
+    li.appendChild(p);
+    li.appendChild(p2);
+    selectedContainer.appendChild(li);
+
+    // const totalCost = document.getElementById('total-cost').innerText;
+    // const toInteger = parseInt(totalCost);
+    // const sum = toInteger + parseInt(price);
+    
+    // const grandTotal = document.getElementById('grand-total').innerText;
+    // const grandToInteger = parseInt(grandTotal);
+    // const sum2 = grandToInteger + parseInt(price);
+
+    console.log();
+    totalCost('total-cost', price);
+    totalCost('grand-total', price);
+    setInnerText('cart-count', count);
+  });
+}
+function totalCost(id, value) {
+    const totalCost = document.getElementById(id).innerText;
+    const toInteger = parseInt(totalCost);
+    const sum = toInteger + parseInt(value);
+    setInnerText(id, sum);
+}
+function grandTotalCost(category) {
+    const totalCost = document.getElementById('total-cost').innerText;
+    const toInteger = parseInt(totalCost);
+
+    if(category == 'bus') {
+        setInnerText('grand-total', toInteger + 100);
+    } else if (category == 'train') {
+        setInnerText('grand-total', toInteger - 200);
+    } else if (category == 'flight') {
+        setInnerText('grand-total', toInteger + 500);
+    } else {
+        setInnerText('grand-total', toInteger);
+    }
+
+    
+}
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}
